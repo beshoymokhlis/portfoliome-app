e84a2517142f25bcca02219bd9a4e5201a0e87ae
Behoy Mokhlis <beshoymokhlisagaiby@gmail.com>
2026-02-06 13:09:46 +0200

    new project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import  { useRef } from 'react';
 import './contact.css';
 import { MdOutlineEmail } from 'react-icons/md';
 import { RiMessengerLine } from 'react-icons/ri';
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,88 +1,85 @@
-import React from 'react'
-import './projects.css';
-import IMG1 from '../../assets/portfolio1.png';
-import IMG2 from '../../assets/portfolio2.png';
-import IMG3 from '../../assets/portfolio3.png';
-import IMG4 from '../../assets/portfolio4.png';
-import IMG5 from '../../assets/portfolio5.png';
-import IMG6 from '../../assets/portfolio6.png';
-
+import "./projects.css";
+import IMG1 from "../../assets/portfolio1.png";
+import IMG2 from "../../assets/portfolio2.png";
+import IMG3 from "../../assets/portfolio3.png";
+import IMG4 from "../../assets/SAINT GEORGE.png";
+import IMG5 from "../../assets/ASCO.png";
+import IMG6 from "../../assets/portfolio6.png";
 
 const portfolioData = [
   {
     id: 1,
-    image: IMG1,
-    title: 'Shopping',
-    github: 'https://github.com/beshoymokhlis/Shopping',
-    demo: 'https://shopping-kohl.vercel.app/'
+    image: IMG4,
+    title: "SAINT GEORGE MARKET",
+    github: "https://github.com/beshoymokhlis/e-commerse-market-main",
+    demo: "https://saintgeorgemarketplace.com/",
   },
   {
     id: 2,
-    image: IMG2,
-    title: 'OMONAC',
-    github: 'https://github.com/beshoymokhlis/OMONAC',
-    demo: ' https://beshoymokhlis.github.io/OMONAC/'
+    image: IMG5,
+    title: "ASCO",
+    github: "https://github.com/beshoymokhlis/ASCO",
+    demo: " https://ascocontracting.com/",
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Digital',
-    github: 'https://github.com/beshoymokhlis/digital-',
-    demo: ' https://beshoymokhlis.github.io/digital-/'
+    title: "Digital",
+    github: "https://github.com/beshoymokhlis/digital-",
+    demo: " https://beshoymokhlis.github.io/digital-/",
   },
   {
     id: 4,
-    image: IMG4,
-    title: 'Coffee',
-    github: 'htgithub.com/project4',
-    demo: 'httpsdemocom'
+    image: IMG1,
+    title: "Shopping",
+    github: "https://github.com/beshoymokhlis/Shopping",
+    demo: "https://shopping-kohl.vercel.app/",
   },
   {
     id: 5,
-    image: IMG5,
-    title: 'Portfolio',
-    github: 'httpithub.com/project5',
-    demo: 'httpmo5.com'
+     image: IMG2,
+    title: "OMONAC",
+    github: "https://github.com/beshoymokhlis/OMONAC",
+    demo: " https://beshoymokhlis.github.io/OMONAC/",
   },
   {
     id: 6,
     image: IMG6,
-    title: 'code',
-    github: 'httpub.com/project6',
-    demo: 'http6.com'
-  }
+    title: "code",
+    github: "httpub.com/project6",
+    demo: "http6.com",
+  },
 ];
-
 
 function Projects() {
   return (
-    <section className='projects' id='projects'>
-      <div className='top_section'>
+    <section className="projects" id="projects">
+      <div className="top_section">
         <h2>Projects</h2>
       </div>
 
-      <div className='container projects_container'>
-
-        {portfolioData.map(({ id, image, title, github, demo }) =>
-          <article key={id} className='Portfolio_item'>
-            <div className='Portfolio_item_img'>
-              <img src={image} alt='project1' />
+      <div className="container projects_container">
+        {portfolioData.map(({ id, image, title, github, demo }) => (
+          <article key={id} className="Portfolio_item">
+            <div className="Portfolio_item_img">
+              <img src={image} alt="project1" />
             </div>
 
             <h3>{title}</h3>
-  
+
             <div className="Portfolio_item_btns">
-              <a href={github} className='btn'>Github</a>
-              <a href={demo} className='btn btn-primary'>View Project</a>
+              <a href={github} className="btn">
+                Github
+              </a>
+              <a href={demo} className="btn btn-primary">
+                View Project
+              </a>
             </div>
-
           </article>
-        )}
-
+        ))}
       </div>
-
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
